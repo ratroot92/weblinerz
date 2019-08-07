d1533cbcd9c35f87fdbe6cdc21d972ebdaa4ea8e
ratroot92 <zcomisb.007@gmail.com>
2019-08-07 20:56:39 +0500

    7 aug last

--- app/employee_model.php
+++ app/employee_model.php
@@ -7,6 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class employee_model extends Model
 {
 
-    protected $table='employee';
-   
+
+
+
+    Schema::create('employees', function($table)
+{
+    $table->bigInteger('e_id');
+    $table->string('e_name');
+    $table->string('e_email');
+    $table->string('e_contract');
+    $table->date('from_date');
+    $table->date('to_date');
+    $table->bigInteger('e_salary');
+
+});
+
 }
